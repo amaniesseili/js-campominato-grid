2474118bb1e47789cfd26cfe9f44513fb69bcf86
amaniesseili <esseiliamani@hotmail.com>
2023-07-12 20:23:54 +0200

    grid da sistemare -troppo grande

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict"
 //campo minato 
 //L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 //Ogni cella ha un numero progressivo, da 1 a 100.
@@ -6,10 +7,33 @@
 //la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 
-var grid = document.getElementById("container-grid");
-for (var i = 1; i <= 100; i++) {
-  var cell = document.createElement("div");
-  
-  cell.className = "cell";
-  grid.appendChild(cell);
-}
+
+const squareCountsSelect = document.querySelector("[name='squareCounts']");
+const BtnStart =document.querySelector("#btn-start");
+const GridContainer = document.querySelector(".grid-container");
+
+
+BtnStart.addEventListener("click", function({}){         // al click sul pulsante
+  const squareCounts = parseInt(squareCountsSelect.value);// leggo il contenuto della select (squareCountsSlelect)
+
+  console.log("valore scelto", squareCounts);
+
+  for(let i = 0; i < squareCounts; i++){     //faccio un ciclo in base al numero scelto dal l'utente
+    
+    const square = document.createElement("div"); // creo l'elemento html
+    const squaresPerRow = Math.sqrt(squareCounts);//  quanto quadrati inseriti per riga
+
+    square.classList.add("grid-square");   // aggungo ad al square la classe +
+    square.textContent = i.toString();                   // +il contenuto 
+    square.style.flexBasis =`calc(100% / ${squaresPerRow})`;
+
+    square.addEventListener("click", function( ){     //+ l'evento 
+      square.classList.toggle("bg-success");          // la classe 
+
+    });
+
+    GridContainer.append(square);
+    
+
+  }
+});
